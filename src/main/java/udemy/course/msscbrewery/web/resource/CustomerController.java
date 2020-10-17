02460d29382f1ea634cdbcd0db56f894e7b072c1
Smit <smitshah95@hotmail.com>
2020-10-17 20:50:17 +0800

    Added Logger

--- src/main/java/udemy/course/msscbrewery/web/resource/CustomerController.java
+++ src/main/java/udemy/course/msscbrewery/web/resource/CustomerController.java
@@ -2,6 +2,7 @@ package udemy.course.msscbrewery.web.resource;
 
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import udemy.course.msscbrewery.service.CustomerService;
 import udemy.course.msscbrewery.web.model.CustomerDto;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/customer")
@@ -25,22 +27,26 @@ public class CustomerController {
 
   @GetMapping("/{customerId}")
   public ResponseEntity<CustomerDto> getCustomer(@PathVariable UUID customerId) {
+    log.info("getCustomer {}", customerId.toString());
     return ResponseEntity.ok(customerService.getCustomer(customerId));
   }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public void addCustomer(@RequestBody CustomerDto customerDto) {
+    log.info("addCustomer {}", customerDto.toString());
     customerService.addCustomer(customerDto);
   }
 
   @PutMapping("/{customerId}")
   public void updateCustomer(@PathVariable UUID customerId, @RequestBody CustomerDto customerDto) {
+    log.info("updateCustomer {}", customerId.toString());
     customerService.updateCustomer(customerId, customerDto);
   }
 
   @DeleteMapping("/{customerId}")
   public void deleteCustomer(@PathVariable UUID customerId) {
+    log.info("deleteCustomer {}", customerId.toString());
     customerService.deleteCustomer(customerId);
   }
 
