3c193e9547064fba3e180dbf5078f2ee48d6b7e4
Smit <smitshah95@hotmail.com>
2020-10-17 10:50:21 +0800

    Updated logger and server port

--- src/main/java/udemy/course/msscbrewery/web/resource/BeerController.java
+++ src/main/java/udemy/course/msscbrewery/web/resource/BeerController.java
@@ -2,6 +2,7 @@ package udemy.course.msscbrewery.web.resource;
 
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import udemy.course.msscbrewery.service.BeerService;
 import udemy.course.msscbrewery.web.model.BeerDto;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/beer")
@@ -25,22 +27,26 @@ public class BeerController {
 
   @GetMapping("/{beerId}")
   public ResponseEntity<BeerDto> getBeer(@PathVariable UUID beerId) {
+    log.info("getBeer {}", beerId.toString());
     return ResponseEntity.ok(beerService.getBeer(beerId));
   }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public void addBeer(@RequestBody BeerDto beerDto) {
+    log.info("addBeer {}", beerDto);
     beerService.addBeer(beerDto);
   }
 
   @PutMapping("/{beerId}")
   public void updateBeer(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+    log.info("update Beer with id {} ", beerId, beerDto);
     beerService.updateBeer(beerId, beerDto);
   }
 
   @DeleteMapping("/{beerId}")
   public void deleteBeer(@PathVariable UUID beerId) {
+    log.info("deleting beer {}", beerId);
     beerService.deleteBeer(beerId);
   }
 }
