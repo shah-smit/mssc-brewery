34d1ee37e8b31b3bbb7d55ead15a3c787b131b1b
Smit <smitshah95@hotmail.com>
2020-10-18 12:59:09 +0800

    Updated Beer DTO with new attributes

--- src/main/java/udemy/course/msscbrewery/web/model/BeerDto.java
+++ src/main/java/udemy/course/msscbrewery/web/model/BeerDto.java
@@ -1,9 +1,12 @@
 package udemy.course.msscbrewery.web.model;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.UUID;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,7 +18,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class BeerDto {
   @Null private UUID id;
+  @Null private Long version;
+  @Null private Timestamp createdDate;
+  @Null private Timestamp lastModifiedDate;
   @NotBlank private String beerName;
   @NotBlank private String beerStyle;
   @Positive private Long upc;
+  @Positive private BigDecimal price;
+  @PositiveOrZero private Integer minOnHand;
+  @PositiveOrZero private Integer quantityToBrew;
 }
--- src/main/java/udemy/course/msscbrewery/web/resource/MvcExceptionHandler.java
+++ src/main/java/udemy/course/msscbrewery/web/resource/MvcExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,5 +23,10 @@ public class MvcExceptionHandler {
                         + constraintViolation.getMessage()));
 
     return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(BindException.class)
+  public ResponseEntity<List> validationBindHandler(BindException e) {
+    return new ResponseEntity(e.getAllErrors(), HttpStatus.BAD_REQUEST);
   }
 }
